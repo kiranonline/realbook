567e359531710872067695a55b902afe9f505796
Soumyojyoti Majumdar <soumyojyoti@redelegant.com>
2019-05-08 21:29:49 +0530

    added invoice rule form componenet and route

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import Form from './form';
+import InvoiceRuleform from './invoicerule/form';
 
 export default class App extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class App extends Component {
         {/* <Route exact path="/" component={()=>(<div>RealBooks</div>)} /> */}
         <Route exact path="/local/booking" component={Form} />
         <Route exact path="/local/booking/:ra_reference" component={Form} />
+        <Route exact path="/invoicerule/form" component={InvoiceRuleform} />
       </Router>
     )
   }
---
+++ front/src/invoicerule/form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class InvoiceRuleform extends Component {
+  render() {
+    return (
+      <div>
+            Invoice Rule Form
+      </div>
+    )
+  }
+}
