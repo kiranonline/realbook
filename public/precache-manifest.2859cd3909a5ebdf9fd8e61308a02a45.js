ea95ddbbbefee3ebb980fc512e9565804cffef88
SoumyoNawab8 <gtechxd@gmail.com>
2019-07-15 12:34:00 +0530

    updated fixes

---
+++ public/precache-manifest.2859cd3909a5ebdf9fd8e61308a02a45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67cf3df8315a35186427def895621315",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a3793503868e5400972",
+    "url": "/static/css/2.a414b599.chunk.css"
+  },
+  {
+    "revision": "60b8155f6977029d89fe",
+    "url": "/static/css/main.594759ac.chunk.css"
+  },
+  {
+    "revision": "4a3793503868e5400972",
+    "url": "/static/js/2.210a666c.chunk.js"
+  },
+  {
+    "revision": "60b8155f6977029d89fe",
+    "url": "/static/js/main.99542dbc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
